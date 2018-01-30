95e91e0a70694f2a0b83220ad3e97e19667ce8d1
Slavomira Stefkova <ss4314@ss4314-laptop.hep.ph.ic.ac.uk>
2018-01-30 19:51:43 +0000

    added trigger selection

---
+++ figs/sel/copy.sh
@@ -0,0 +1,2 @@
+cp /vols/lhcb/ss4314/Gauss_v45r9/ParticleGun/higgstesting/reporttrialqpres_new.pdf .
+cp /vols/lhcb/ss4314/Gauss_v45r9/ParticleGun/higgstesting/reporttry_new.pdf .
---
+++ figs/sel/copy_mcmine.sh
@@ -0,0 +1,4 @@
+
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/ananote2/022/figs/realmc/* .
+
+
