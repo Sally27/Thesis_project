ccf095f132a8dee1c9ef31416a439bb2bac11674
Slavomira Stefkova <ss4314@ss4314-laptop.hep.ph.ic.ac.uk>
2018-01-31 19:53:00 +0000

    qsq selection

---
+++ figs/sel/copy_qsqsel.sh
@@ -0,0 +1,5 @@
+
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/plotting_tools/plot_q2choice/bin/scatterplotiatko_qmin.pdf .
+
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/plotting_tools/plot_q2choice/bin/scatterplotiatko_qmin_data.pdf .
+
