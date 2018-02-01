d96c82643fd4e4caab086bc80c8330d510d4a67c
Slavomira Stefkova <ss4314@ss4314-laptop.hep.ph.ic.ac.uk>
2018-02-01 19:46:58 +0000

    finished pid muon extra chapter

--- copy.sh
+++ copy.sh
@@ -1,1 +1,2 @@
 cp -r * ../backup_Thesis
+scp -r * ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/Thesis/
---
+++ figs/trimuon/copy_nshared.sh
@@ -0,0 +1,2 @@
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/ananote2/022/figs/nSharedComparison/*pdf .
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/ananote2/022/figs/compareRun1and2016selection/*pdf .
