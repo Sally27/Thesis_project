d5047453afc61675444937479a9fd300e114fb9f
Slavomira Stefkova <ss4314@ss4314-laptop.hep.ph.ic.ac.uk>
2018-02-02 19:30:45 +0000

    clone chapter

---
+++ figs/trimuon/copy_clones.sh
@@ -0,0 +1,4 @@
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/preparetuplesforBDTfinalallvartrigger/ThreeSameSignMuonDatapreparetupleNew/bin/TTtuple/bin/compnice_even_nicer_ONLY_STACKED_HIST_CorrM.pdf .
+
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/preparetuplesforBDTfinalallvartrigger/ThreeSameSignMuonDatapreparetupleNew/bin/TTtuple/bin/compnice_even_nicer_ONLY_STACKED_HIST_VisibleCorrM.pdf .
+
