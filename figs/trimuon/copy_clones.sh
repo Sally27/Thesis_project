a7a6b30b8ace87c0ad2390eee2131843c77cd029
Slavomira Stefkova <ss4314@ss4314-laptop.hep.ph.ic.ac.uk>
2018-02-06 19:24:55 +0000

    added my pid sample

--- figs/trimuon/copy_clones.sh
+++ figs/trimuon/copy_clones.sh
@@ -3,13 +3,18 @@
 #scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/preparetuplesforBDTfinalallvartrigger/ThreeSameSignMuonDatapreparetupleNew/bin/TTtuple/bin/compnice_even_nicer_ONLY_STACKED_HIST_VisibleCorrM.pdf .
 
 
-scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/plotting_tools/plot_clones/bin/VertexInfoClone.pdf .
+#scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/plotting_tools/plot_clones/bin/VertexInfoClone.pdf .
 
-scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/plotting_tools/plot_clones/bin/VertexInfoNoClone.pdf .
+#scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/plotting_tools/plot_clones/bin/VertexInfoNoClone.pdf .
 
 #scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/plotting_tools/plot_clones/bin/OTxandyClone.pdf .
 
 #scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/plotting_tools/plot_clones/bin/OTxandyNoClone.pdf .
+
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/preparetuplesforBDTfinalallvartrigger/ThreeSameSignMuonDatapreparetupleNew/bin/TTtuple/bin/compnice_even_nicer_ONLY_STACKED_HIST_oneFILE_BplusDiraclone.pdf .
+
+
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/preparetuplesforBDTfinalallvartrigger/ThreeSameSignMuonDatapreparetupleNew/bin/TTtuple/bin/compnice_even_nicer_ONLY_STACKED_HIST_oneFILE_BplusDiranoclone.pdf .
 
 #scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/preparetuplesforBDTfinalallvartrigger/ThreeSameSignMuonDatapreparetupleNew/bin/TTtuple/bin/compnice_even_nicer_ONLY_STACKED_HIST_oneFILE_sumnSharedclone.pdf .
 
