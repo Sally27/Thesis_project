499bc47678208d3875fc983f1609496e26197f8e
Slavomira Stefkova <ss4314@ss4314-laptop.hep.ph.ic.ac.uk>
2018-02-16 19:07:49 +0000

    added combinatorial

---
+++ figs/trimuon/jpsikst/2012/copy_nice_color.sh
@@ -0,0 +1,4 @@
+
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/fitjpsikst/fitjpsikst_idPION_includeswaps_automatize_master_sweight_cinMuonAc_NOghostprob_mydefbin_onnlyONEbinINeta/NOFCME/workspace/plotJpsiKstFitLogyPretty_nicecolor.pdf plotJpsiKstFitLogyPretty_nicecolor_2012_KAONMISID.pdf 
+
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/fitjpsikst/fitjpsikst_idKAON_includeswaps_automatize_master_sweight_cinMuonAc_NOghostprob_mydefbin_onnlyONEbinINeta/NOFCME/workspace/plotJpsiKstFitLogyPretty_nicecolor.pdf plotJpsiKstFitLogyPretty_nicecolor_2012_PIONMISID.pdf 
