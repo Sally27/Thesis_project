2893d3e22fa12cf7987bb44570b089403e32b7b6
Slavomira Stefkova <ss4314@ss4314-laptop.hep.ph.ic.ac.uk>
2018-03-01 18:04:32 +0000

    finished correcting ulriks comments for PID chapter

--- figs/trimuon/copy_nshared.sh
+++ figs/trimuon/copy_nshared.sh
@@ -1,2 +1,6 @@
-scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/ananote2/022/figs/nSharedComparison/*pdf .
-scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/ananote2/022/figs/compareRun1and2016selection/*pdf .
+#scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/plotting_tools/plot_before_nSharedcut/bin/plotvariablewantlogtruemu1_nSharedJPSIKDATA.pdf .
+
+
+#scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/plotting_tools/plot_before_nSharedcut/bin/plotvariablewantlogtruemu1_nSharedJPSIKMC.pdf .
+
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/probnnRun2tuning/investigation/investigatenshared_removedll/attemptomakeitintodir/bin/final_pretty_2016_pion.pdf .
