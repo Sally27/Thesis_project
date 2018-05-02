e1bafc2a2429a85374d5e745f042d3331b4819df
Slavomira Stefkova <ss4314@ss4314-laptop.hep.ph.ic.ac.uk>
2018-05-02 22:47:29 +0100

    up to misid

---
+++ figs/efficiency/signalfits/partrecomc/copy_here.sh
@@ -0,0 +1,9 @@
+
+scp -r ss4314@lx00.hep.ph.ic.ac.uk:/home/hep/ss4314/public_html/fitsignal/fitsignal_simultaneous_final_GenLevelEff_CorrectMisid_niceplotsincluded/probnnmu035probmu35/workspace_preparation/SimPartReco_veryniceplot_paramon_paperHighFCME.pdf simprhighfcmeRun1nolog.pdf
+
+
+scp -r ss4314@lx00.hep.ph.ic.ac.uk:/home/hep/ss4314/public_html/fitsignal/fitsignal_simultaneous_final_GenLevelEff_CorrectMisid_niceplotsincluded/probnnmu035probmu35/workspace_preparation/SimPartReco_veryniceplot_paramon_paperLowFCME.pdf simprlowfcmeRun1nolog.pdf
+
+scp -r ss4314@lx00.hep.ph.ic.ac.uk:/home/hep/ss4314/public_html/fitsignal/fitsignal_final_GenLevelEff_CorrectMisid_niceplotsincluded/probnnmu035probmu35/workspace_preparation/SimPartReco_veryniceplot_paramon_paper.pdf simprnofcmeRun1nolog.pdf
+
+
