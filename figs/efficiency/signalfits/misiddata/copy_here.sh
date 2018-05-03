1eed1e660c2ebd308c891e869728fd958696f8fc
Slavomira Stefkova <ss4314@ss4314-laptop.hep.ph.ic.ac.uk>
2018-05-03 19:42:39 +0100

    up to fitting blinded fit

---
+++ figs/efficiency/signalfits/misiddata/copy_here.sh
@@ -0,0 +1,9 @@
+
+scp -r ss4314@lx00.hep.ph.ic.ac.uk:/home/hep/ss4314/public_html/fitsignal/fitsignal_simultaneous_final_GenLevelEff_CorrectMisid_niceplotsincluded/probnnmu035probmu35/workspace_preparation/DataMisid_veryniceplot_paramon_paperHighFCME.pdf misiddatahighfcmeRun1nolog.pdf
+
+
+scp -r ss4314@lx00.hep.ph.ic.ac.uk:/home/hep/ss4314/public_html/fitsignal/fitsignal_simultaneous_final_GenLevelEff_CorrectMisid_niceplotsincluded/probnnmu035probmu35/workspace_preparation/DataMisid_veryniceplot_paramon_paperLowFCME.pdf misiddatalowfcmeRun1nolog.pdf
+
+scp -r ss4314@lx00.hep.ph.ic.ac.uk:/home/hep/ss4314/public_html/fitsignal/fitsignal_final_GenLevelEff_CorrectMisid_niceplotsincluded/probnnmu035probmu35/workspace_preparation/DataMisid_veryniceplot_paramon_paper.pdf misiddatanofcmeRun1nolog.pdf
+
+
