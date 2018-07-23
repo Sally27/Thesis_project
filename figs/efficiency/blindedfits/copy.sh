af9f3b231e9b13c36a8740d821cb70c4681c534e
Slavomira Stefkova <ss4314@ss4314-laptop.hep.ph.ic.ac.uk>
2018-07-23 18:58:54 +0100

    parametrisation of the fit started

---
+++ figs/efficiency/blindedfits/copy.sh
@@ -0,0 +1,5 @@
+scp ss4314@lx00.hep.ph.ic.ac.uk:/home/hep/ss4314/public_html/fitsignal/fitsignal_simultaneous_final_genlevelcor_allsyst/probnnmu035probmu35/workspace_plots/HighFCMEprobnnmu035probmu35plotB23MuNuFitPretty_chi2_reallynice.pdf .
+
+scp ss4314@lx00.hep.ph.ic.ac.uk:/home/hep/ss4314/public_html/fitsignal/fitsignal_simultaneous_final_genlevelcor_allsyst/probnnmu035probmu35/workspace_plots/LowFCMEprobnnmu035probmu35plotB23MuNuFitPretty_chi2_reallynice.pdf .
+
+scp ss4314@lx00.hep.ph.ic.ac.uk:/home/hep/ss4314/public_html/fitsignal/fitsignal_final_genlevelcor_allsyst/probnnmu035probmu35/workspace_plots/probnnmu035probmu35plotB23MuNuFitPretty_chi2_reallynice.pdf .
