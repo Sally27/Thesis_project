87529aed5151420fa84d31988d581710b9df0dcc
Slavomira Stefkova <ss4314@ss4314-laptop.hep.ph.ic.ac.uk>
2018-07-25 19:03:13 +0100

    added cls

---
+++ figs/efficiency/expe_sensitivity/copy.sh
@@ -0,0 +1,8 @@
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/CLsMethod/expected_limit_NOTsim/plotExclusionLimit/bin/ClsExclusion.pdf  AllNotSim_partial.pdf
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/CLsMethod/expected_limit_NOTsim_allsyst/plotExclusionLimit/bin/ClsExclusion.pdf AllNotSim_full.pdf
+
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/CLsMethod/expected_limit_sim/plotExclusionLimit/bin/ClsExclusion.pdf  AllSim_partial.pdf
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/CLsMethod/expected_limit_sim_allsyst/plotExclusionLimit/bin/ClsExclusion.pdf AllSim_full.pdf
+
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/CLsMethod/expected_limit_sim_Run1only/plotExclusionLimit/bin/ClsExclusion.pdf Run1onlySim_partial.pdf
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/CLsMethod/expected_limit_sim_Run1only_allsyst/plotExclusionLimit/bin/ClsExclusion.pdf Run1onlySim_full.pdf
---
+++ figs/efficiency/systematics/signalbias/copy.sh
@@ -0,0 +1,2 @@
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/systematic_uncertainties/fitbias/NOTSimultaneous/BR_1_0_e_8/SignalBias_signal_numoftoys_10000_injectsig1e-08.pdf notsim.pdf
+scp ss4314@lx00.hep.ph.ic.ac.uk:/vols/lhcb/ss4314/systematic_uncertainties/fitbias/Simultaneous_BR/toy_ass_10000_1_0e_8/SignalBias_signal_numoftoys_10000_injectsig1e-08.pdf sim.pdf
