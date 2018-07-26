d5ce0f5c4c5c9f4504d8422d2e1ee6156be85476
Slavomira Stefkova <ss4314@ss4314-laptop.hep.ph.ic.ac.uk>
2018-07-26 15:46:40 +0100

    added conclusion

---
+++ figs/results/copy.sh
@@ -0,0 +1,6 @@
+
+
+
+scp ss4314@lx00.hep.ph.ic.ac.uk:/home/hep/ss4314/public_html/CLsmethod/expected_limit_sim_allsyst_upgrade_unblinding/smoothing/plotExclusionLimit/bin/ClsExclusion_new_Log_picked.pdf .
+scp ss4314@lx00.hep.ph.ic.ac.uk:/home/hep/ss4314/public_html/CLsmethod/expected_limit_sim_allsyst_upgrade_unblinding/smoothing/plotExclusionLimit/bin/ClsExclusion_new_picked_final.pdf .
+
---
+++ figs/results/copy_projection.sh
@@ -0,0 +1,1 @@
+cp /home/ss4314/cernbox/B23MuNuPaper_v2r0/B23MuNuPaper/LHCb-latex-template/latest/latex/figs/fit.pdf .
